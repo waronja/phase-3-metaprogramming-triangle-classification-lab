8ec56595384a8deafe3ccb96ac647b69d0a15d93
simon waronja <simon.waronja@student.moringaschool.com>
2022-08-29 16:16:34 +0300

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+
+  end
+
+  def kind
+      if @a <= 0 ||  @b <= 0 || @c <= 0
+        raise TriangleError
+      end 
+
+      if @a + @b <= @c  || @a + @c <= @b ||  @b + @c <= @a
+        raise TriangleError
+      end 
+
+      return :equilateral if @a == @b && @b == @c
+      return :isosceles   if @a == @b || @a == @c ||  @b == @c
+      return :scalene     if @a != @b && @a != @c &&  @b != @c 
+
+    end
+ 
+ class TriangleError < StandardError
+  end
 end
